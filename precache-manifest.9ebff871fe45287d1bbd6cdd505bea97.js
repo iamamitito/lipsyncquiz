0b1439722761ec0128b51d80c5ac00cf683c7412
Héctor Espinoza <hespinoza@uchile.cl>
2020-08-04 17:46:24 -0400

    Updates

---
+++ precache-manifest.9ebff871fe45287d1bbd6cdd505bea97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68345d77253fa885f725720ca4baa4cd",
+    "url": "/lipsyncquiz/index.html"
+  },
+  {
+    "revision": "a3076cac073e78bb4405",
+    "url": "/lipsyncquiz/static/js/2.5d7a1127.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lipsyncquiz/static/js/2.5d7a1127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98db90e6c1a0b90a8a8e",
+    "url": "/lipsyncquiz/static/js/main.6aece20a.chunk.js"
+  },
+  {
+    "revision": "98d0b0bd7160fbffbb7e",
+    "url": "/lipsyncquiz/static/js/runtime-main.13a0ca96.js"
+  },
+  {
+    "revision": "9b4f9f6a6c1c12eaae33b191ae111049",
+    "url": "/lipsyncquiz/static/media/spinner.9b4f9f6a.gif"
+  }
+]);
