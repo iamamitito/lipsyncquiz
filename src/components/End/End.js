05a268dce7865ce7a8cba2ad516e5c3f8126627e
Héctor Espinoza <hespinoza@uchile.cl>
2020-08-04 17:59:00 -0400

    routes fixed

--- src/components/End/End.js
+++ src/components/End/End.js
@@ -6,7 +6,7 @@ const End = ({ score }) => {
             <h1>Score: </h1>
             <span className="text-pink-600 text-4xl ">{score}</span>
             <span className="text-lg w-full font-light">{score > 80 ? "You're a winner, baby!" : "Learn your herstory, sis!"}</span>
-            <a href="/" className="bg-pink-500 my-3 py-2 px-4 shadow hover:bg-pink-600 rounded"><i className="fas fa-redo"></i></a>
+            <a href="/lipsyncquiz" className="bg-pink-500 my-3 py-2 px-4 shadow hover:bg-pink-600 rounded"><i className="fas fa-redo"></i></a>
         </div>)
 }
 export default End;
