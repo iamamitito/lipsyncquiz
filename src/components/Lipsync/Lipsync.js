ec1afa4736e9ed3b63328962d488eff0010a90a5
Héctor Espinoza <hespinoza@uchile.cl>
2020-08-05 15:45:08 -0400

    fixed API mistakes

--- src/components/Lipsync/Lipsync.js
+++ src/components/Lipsync/Lipsync.js
@@ -5,22 +5,23 @@ const Lipsync = ({ lipsync, queens, handleAnswer, showAnswers, nextLipsync }) =>
     return (
         <div className="lipsync w-11/12 lg:w-1/2 m-auto">
             <div className="bg-white text-pink-600 p-5 flex flex-col rounded-md shadow-md text-center">
-                <h2 className="text-2xl">{lipsync.name}</h2><small>by {lipsync.artist}</small>
+                <h2 className="text-2xl">{lipsync.id === 129 ? "I'm Coming Out" : lipsync.name}</h2><small>by {lipsync.artist}</small>
             </div>
 
             <div className="grid grid-cols-2 gap-6 mt-6">
                 {queens.map(queen => {
                     const textColor = showAnswers ? queen.won ? 'text-green-500' : 'text-red-500' : 'text-pink-600';
+                    const icon = showAnswers ? queen.won ? <i className="fas fa-check"></i> : <i className="fas fa-times"></i> : null;
                     return (<button
                         onClick={() => handleAnswer(queen.won)}
-                        className={`bg-white ${textColor} p-4 rounded-md focus:outline-none shadow-md`}
+                        className={`bg-white p-4 ${textColor} rounded-md focus:outline-none shadow-md`}
                         key={queen.id}>
-                        {queen.name}</button>)
+                        {icon} {lipsync.id === 143 && queen.won ? 'Monet X Change' : queen.name}</button>)
                 })
                 }
             </div>
             {showAnswers && (
-                <button className="float-right bg-pink-500  mt-3 p-3 rounded-md focus:outline-none shadow" onClick={nextLipsync} >Continue <i className="fas fa-chevron-right"></i></button>
+                <button className="float-right bg-pink-500  mt-4 p-3 rounded-md focus:outline-none shadow" onClick={nextLipsync} >Next <i className="fas fa-chevron-right"></i></button>
             )}
         </div>
     );
